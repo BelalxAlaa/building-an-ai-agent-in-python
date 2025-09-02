b1a8fa4d7b300290e08c43aaa4e629e6aa117417
BelalxAlaa <belalsteam@gmail.com>
2025-09-02 14:36:46 +0300

    Finished Writing to a file

--- tests.py
+++ tests.py
@@ -1,22 +1,16 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
 def test():
 
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' file:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
     
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py' file:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
     
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat' file:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
     
-    result = get_file_content("calculator", "pkg/does_not_exist.py")
-    print("Result for 'pkg/does_not_exist.py' file:")
-    print(result)
     
     
 
