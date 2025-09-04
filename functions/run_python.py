4ef3390dda0eba1403f304f70e340f71579eed1c
BelalxAlaa <belalsteam@gmail.com>
2025-09-04 19:54:30 +0300

    Add initial project files and implement file handling functions
    - Create launch.json and settings.json for VSCode configuration.
    - Implement run_python_file function to execute Python scripts with arguments.
    - Add get_files_info, get_file_content, and write_file functions for file operations.
    - Update main.py to integrate with the new functions and handle user prompts.
    - Enhance tests.py to validate the functionality of the implemented features.

---
+++ functions/run_python.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+
+    if not abs_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+    try:
+        completed_process = subprocess.run(
+            ["python", abs_file_path, *args], timeout=30, capture_output=True
+        )
+        completed_process.stdout = completed_process.stdout.decode() 
+        if completed_process.stdout == "":
+            return "No output produced."
+
+        formatted_output = f"""STDOUT:\n{completed_process.stdout}
+STDERR:\n{completed_process.stderr.decode()}
+{f"Process exited with code {completed_process.returncode}" if completed_process.returncode != 0 else ""}
+"""
+
+        return formatted_output
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+
+
+if __name__ == "__main__":
+    run_python_file("calculator", "f.py")
--- main.py
+++ main.py
@@ -1,41 +1,63 @@
 import os
 import sys
+
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from google.genai import Client, types
+
+from functions.get_files_info import available_functions
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    client = Client(api_key=api_key)
 
     if len(sys.argv) < 2:
         print("Error MSG: Prompt is not provided.")
         sys.exit(1)
 
     user_prompt = sys.argv[1]
-    is_verbose = '--verbose' in sys.argv
+    is_verbose = "--verbose" in sys.argv
 
     if is_verbose:
         print(f"User prompt: {user_prompt}")
-    
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)])
-    ]
-
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     generate_content(client, messages, is_verbose)
-    
 
-def generate_content(client, messages, is_verbose):
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+def generate_content(client: Client, messages: list[types.Content], is_verbose: bool):
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
     if is_verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+
+    if response.function_calls is None:
+        print("No functions called")
+        return
     
+    for call in response.function_calls:
+        print(f"Calling function: {call.name}({call.args})")
 
 
 if __name__ == "__main__":
